9a1df78362404a285768a3240047964364fc7563
pydevph <pydevph@gmail.com>
2026-01-14 22:35:55 +0800

    upload

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,8 @@ import StickyCTA from '@/components/StickyCTA';
 import { prisma } from '@/lib/prisma';
 import type { Author, Book, Testimonial } from '@prisma/client';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   let books: Book[] = [];
   let testimonials: Testimonial[] = [];
